7fce9b8f7564de5c1e7698b99f41d05c45729f41
zhishiwanwan01 <609429923@qq.com>
2025-12-17 18:21:55 +0800

    refactor: comment out xhost command in build script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e # 遇到错误立即退出
 
-xhost + # 允许 Docker 容器访问 X11 显示服务器
+# xhost + # 允许 Docker 容器访问 X11 显示服务器
 
 # Auto-detect Windows client IP (SSH source)
 WINDOWS_IP=$(who am i | awk '{print $5}' | sed 's/[()]//g' | cut -d: -f1)
